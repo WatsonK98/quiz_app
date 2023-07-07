64e73a1e0024dd98654d53aa859c5244db1d2ae5
Alexander Watson <aswatson95@gmail.com>
2023-07-06 22:03:53 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() async => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QuizAppLogin',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen,
+          title: const Text('Quiz App Login'),
+        ),
+        body: const QuizAppLogin(),
+
+      ),
+    );
+  }
+}
+
+class QuizAppLogin extends StatefulWidget {
+  const QuizAppLogin({super.key});
+
+  @override
+  State<QuizAppLogin> createState() => _QuizAppLogin();
+}
+
+class _QuizAppLogin extends State<QuizAppLogin> {
+
+  final GlobalKey<FormFieldState<String>> _usernameFormFieldKey = GlobalKey();
+  final GlobalKey<FormFieldState<String>> _passwordFormFieldKey = GlobalKey();
+
+  _notEmpty(String value) => value.isNotEmpty;
+
+  get values => {
+    'username': _usernameFormFieldKey.currentState?.value,
+    'password': _passwordFormFieldKey.currentState?.value
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'CS 5381',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+          ),
+          const Text(
+            'version 1.0.0',
+            style: TextStyle(fontSize: 10),
+          ),
+          TextFormField(
+            key: _usernameFormFieldKey,
+            decoration: const InputDecoration(
+              labelText: 'Username*',
+            ),
+            validator: (value) =>
+            _notEmpty(value!) ? null : 'Username is required',
+          ),
+          TextFormField(
+            key: _passwordFormFieldKey,
+            obscureText: true,
+            decoration:  const InputDecoration(
+              labelText: 'Password**',
+            ),
+            validator: (value) =>
+            _notEmpty(value!) ? null : 'Password is required',
+          ),
+          Builder(builder: (context){
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: (){
+                    if(Form.of(context).validate()){
+                      Text('$values');
+                    }
+                  },
+                  child: const Text('Log In'),
+                ),
+                TextButton(
+                  onPressed: () => Form.of(context).reset(),
+                  child: const Text('Reset'),
+                ),
+              ],
+            );
+          }),
+          const Text(
+            '*User name of your primary email address provided by UTEP.',
+            style: TextStyle(fontSize: 10),
+          ),
+          const Text(
+            '**Password is the last 4 digits of your Student ID.',
+            style: TextStyle(fontSize: 10),
+          ),
+        ],
+      ),
+    );
+  }
+}
