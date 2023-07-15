6d762ced43559b3b1060aaa5a6a1de18e8adae5b
Alexander Watson <aswatson95@gmail.com>
2023-07-15 00:58:26 -0600

    generate quiz

--- lib/userdefine.dart
+++ lib/userdefine.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_me/loading.dart';
 
 class Userdefine extends StatelessWidget{
   final dynamic values;
@@ -19,7 +20,67 @@ class Userdefine extends StatelessWidget{
           backgroundColor: Colors.lightGreen,
           title: const Text('Quiz Builder'),
         ),
+        body: Define(values, finalQuiz),
       ),
+    );
+  }
+}
+
+class Define extends StatefulWidget{
+  final dynamic values;
+  final dynamic finalQuiz;
+
+  const Define(this.values, this.finalQuiz, {super.key});
+
+  @override
+  State<Define> createState() => _Define();
+}
+
+class _Define extends State<Define>{
+
+  final GlobalKey<FormFieldState<String>> _userFormFieldKey = GlobalKey();
+
+  get value => {
+    'user': _userFormFieldKey.currentState?.value
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Enter desired Number of Questions',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            const Text(
+              '1-15 [default: 10]',
+            ),
+            TextFormField(
+              key: _userFormFieldKey,
+              decoration:  const InputDecoration(
+                labelText: 'Desired Questions:',
+              ),
+            ),
+            Builder(builder: (context){
+              return Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if(value['user'] > 0 && value['user'] < 15){
+                      //create a userQuiz
+                      var userQuiz = finalQuiz.generateQuiz(value['user']);
+                    } else {
+                      //create defaultQuiz
+                      var defaultQuiz = finalQuiz.generateQuiz(10);
+                    }
+                  },
+                  child: const Text('Accept'),
+                ),
+              );
+            }),
+          ],
+        )
     );
   }
 }
